82b5759d9483c9532977c95a30dd79b86085f32f
Kubik007 <kubaramuradova1764@gmail.com>
2022-12-01 21:43:09 +0300

    programmistika

---
+++ Tema_2/Задание 1.py
@@ -0,0 +1,16 @@
+# Напишите функцию sum_range(start, end), которая суммирует все целые числа
+# от значения «start» до величины «end» включительно.
+# Если пользователь задаст первое число большее чем второе, просто поменяйте их местами.
+start = int(input('Введите первое число'))
+end = int(input('Введите второе число'))
+
+def sum_range(start, end):
+    if start > end:
+        start, end = end, start
+    k = 0
+    for i in range (start, end+1):
+        k += 1
+    return k
+
+print(sum_range(start, end))
+
